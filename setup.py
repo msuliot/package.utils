eb4d3316f1e5e5ae20be64cb7540ccea411e6429
Suliot <suliot@msn.com>
2024-05-10 10:11:58 -0700

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="msuliot.utils",
+    name="utils",
     version="0.2.0",
     author="Michael Suliot",
     author_email="michael@suliot.com",
